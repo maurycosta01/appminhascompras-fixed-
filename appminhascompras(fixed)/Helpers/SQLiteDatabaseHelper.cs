78a874a08e64dbe585295ce5f3617855273e4524
maurycosta01 <maurycosta14@hotmail.com>
2025-03-11 19:09:29 -0300

    contruçao da classe sqllite

--- appminhascompras(fixed)/Helpers/SQLiteDatabaseHelper.cs
+++ appminhascompras(fixed)/Helpers/SQLiteDatabaseHelper.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using appminhascompras_fixed_.Models;
+using SQLite;
 
 namespace appminhascompras_fixed_.Helpers
 {
-    class SQLiteDatabaseHelper
+    public class SQLiteDatabaseHelper
     {
+        readonly SQLiteAsyncConnection _conn; // variável de conexão com o banco de dados
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait(); // cria a tabela de produtos
+        }
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p); // insere um produto
+        }
+        public Task<List<Produto>> Update(Produto p) //atulizacao da lista
+        {
+            string sql = "UPDATE Produto SET Desscricao=?, Quantidade=?, Preco=? WHERE Id=?";
+            return _conn.QueryAsync<Produto>(sql, p.Descricao,p.Quantidade ,p.preco ,p.Id);
+        }
+        public Task<int> Delete(int id) //deletar produto 
+        {
+            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+        public Task<List<Produto>> GetAll() //pegar todos os produtos
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+        public Task<List<Produto>> Search(string q)
+        {
+            string sql = "SELECT * Produto WHERE descricao LIKE '%"+ q + "%'";
+            return _conn.QueryAsync<Produto>(sql);
+        }
     }
 }
